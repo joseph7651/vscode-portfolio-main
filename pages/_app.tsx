1231f93c2db58cce633f20bb3753d0dfcc19671f
joseph7651 <josephcastellano715@gmail.com>
2025-08-02 00:00:00 +0100

    chore: update app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -32,3 +32,4 @@ export default MyApp;
 # touched on 2025-08-14T19:56:40.080395Z
 # touched on 2025-08-14T19:57:03.366741Z
 # touched on 2025-08-14T19:57:27.582255Z
+# touched on 2025-08-14T19:57:49.759808Z
